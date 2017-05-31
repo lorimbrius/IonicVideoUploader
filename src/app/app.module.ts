0bdc3e7c71a5972ad13b05846aa87c8daf2af0c9
Joshua Armstrong <jarmstrong@nvisia.com>
2017-05-31 10:21:28 -0500

    Can browse for a file now.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { Camera } from '@ionic-native/camera';
 
 @NgModule({
   declarations: [
@@ -26,6 +27,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     ListPage
   ],
   providers: [
+    Camera,
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,56 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Platform } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  public fileName: string;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,
+              public plt: Platform,
+              private camera: Camera) {
 
+  }
+
+  public readonly OPTIONS: CameraOptions = {
+    destinationType: this.camera.DestinationType.FILE_URI,
+    sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
+    allowEdit: true,
+    mediaType: this.camera.MediaType.VIDEO
+  }
+
+  public commandGetVideo(event?: Event): void {
+    if (event && event.preventDefault) {
+      event.preventDefault();
+    }
+
+    this.plt.ready().then((readySource) => {
+      this.camera.getPicture(this.OPTIONS).then((videoData) => {
+        console.log(videoData);
+        this.fileName = videoData;
+      }, (err) => {
+        console.log(err);
+      });
+    });
+  }
+
+  // Going to have to do different things on iOS vs. Android
+  public commandBrowse(event?: Event): string {
+    if (event && event.preventDefault) {
+      event.preventDefault();
+    }
+
+    let fileBrowserResult: string = "";
+
+    if (this.fileName && this.fileName.length > 0) {
+      // start file browser in specified location
+    }
+
+    return fileBrowserResult;
   }
 
 }
